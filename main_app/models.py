c7453b34c3eca9b5b8d4b6812966ce3f9fa09202
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-22 15:33:47 -0700

    camera model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Camera(models.Model): 
+    make = models.CharField(max_length=30)
+    model = models.CharField(max_length=50)
+    body_type = models.CharField(max_length=50)
+    lens_mount = models.CharField(max_length=50)
+    sensor_size = models.CharField(max_length=50)
+    sensor_mp = models.DecimalField(max_digits=5, decimal_places=2)
