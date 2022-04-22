d5799d73a543269201f498cc71095094e550cd3d
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-22 14:11:48 -0700

    about view

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Welcome to KitBuilder!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About KitBuilder</h1>')
+    return render(request, 'about.html')
