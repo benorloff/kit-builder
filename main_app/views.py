e53e6d95e76ecd3d53a1f9544f4e1eb3e743a897
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-22 14:44:18 -0700

    camera class and mock data (will be replaced by model):

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+class Camera:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, make, model, body_type, lens_mount, sensor_size, sensor_mp = 0):
+    self.make = make
+    self.model = model
+    self.body_type = body_type
+    self.lens_mount = lens_mount
+    self.sensor_size = sensor_size
+    self.sensor_mp = sensor_mp
+
+cameras = [
+  Camera('Nikon', 'Z9', 'SLR-style mirrorless', 'Nikon Z', 'Full frame', 46),
+  Camera('Leica', 'M11', 'Large SLR', 'Leica M', 'Full frame', 60),
+  Camera('Sony', 'a7 IV', 'SLR-style mirrorless', 'Sony E', 'Full frame', 33),
+  Camera('Fujifilm', 'GFX 100S', 'SLR-style mirrorless', 'Fujifilm G', 'Medium format', 102),
+  Camera('Canon', 'EOS R3', 'SLR-style mirrorless', 'Canon RF', 'Full frame', 24),
+]
 
 def home(request):
     return HttpResponse('<h1>Welcome to KitBuilder!</h1>')
