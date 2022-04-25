34006c4acd596a3cd3b52dcb7a25a5c9e092cf09
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-25 14:32:29 -0700

    login view

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ app_name = 'main_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
     path('about/', views.about, name='about'),
     path('cameras/', views.cameras_index, name='index'),
     path('cameras/<int:camera_id>/', views.cameras_detail, name='detail'),
