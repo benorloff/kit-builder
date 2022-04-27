64e1c9e91179b3980dda52b1bc3ca82d6bc3493b
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-27 13:25:35 -0700

    rename kit.lenses to kit.lens

--- main_app/models.py
+++ main_app/models.py
@@ -83,7 +83,7 @@ class Lens(models.Model):
 class Kit(models.Model):
     name = models.CharField(max_length=255)
     cameras = models.ManyToManyField(Camera)
-    lenses = models.ManyToManyField(Lens)
+    lens = models.ManyToManyField(Lens)
     def __str__(self):
         return (f"{self.name}")
     def get_absolute_url(self):
