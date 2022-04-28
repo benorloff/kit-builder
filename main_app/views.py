fcd2924d9243c2dc59d739bd4c0b9adcf3b61073
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-04-28 14:04:48 -0700

    add signup view

--- main_app/views.py
+++ main_app/views.py
@@ -8,12 +8,26 @@ from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteVi
 from .forms import NewUserForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from .models import Kit, Camera, Lens
 
 def home(request):
     return HttpResponse('<h1>Welcome to KitBuilder!</h1>')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 def register_request(request):
     if request.method == 'POST':
